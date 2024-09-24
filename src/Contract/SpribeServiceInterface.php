ddee66ef5a6dc8fcecf8d496bc3a330ca5886c79
q90016200 <q90016200@gmail.com>
2024-09-24 10:37:41 +0800

    update

--- src/Contract/SpribeServiceInterface.php
+++ src/Contract/SpribeServiceInterface.php
@@ -8,14 +8,13 @@ interface SpribeServiceInterface
      * 遊戲啟動
      *
      * @param string $op_code
-     * @param string $vendor_code
      * @param string $account
      * @param string $game_code
      * @param string $language
      * @param string $odd_type
      * @return mixed
      */
-    function gameLaunch(string $op_code, string $vendor_code, string $account, string $game_code, string $language, string $odd_type);
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $odd_type);
 
     /**
      * 遊戲詳情
@@ -31,19 +30,17 @@ interface SpribeServiceInterface
      * 抓取遊戲紀錄
      *
      * @param string $op_code
-     * @param string $vendor_code
      * @param integer $past_minutes
      * @param string $cache_key
      * @return mixed
      */
-    function betLogGrabber(string $op_code, string $vendor_code, int $past_minutes, string $cache_key);
+    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
 
     /**
      * 全營商 抓取遊戲紀錄
      *
-     * @param string $vendor_code
      * @param integer $past_minutes
      * @return mixed
      */
-    function betLogGrabberAll(string $vendor_code, int $past_minutes);
+    function betLogGrabberAll(int $past_minutes);
 }
